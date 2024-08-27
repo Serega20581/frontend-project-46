49a0a536c447411d253f471d72cec89b8baf7e91
SergeiToptunov <toptunov.2005@mail.ru>
2024-08-27 13:45:23 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,9 +9,12 @@ program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
   .version("1.0.0")
-  .option("-f, --format <type>", "output format", "stylish")
-  .action((filepath1, filepath2, options) => {
-    const diff = genDiff(filepath1, filepath2, options.format);
-    console.log(diff);
-  })
-  .parse();
+  .arguments("<filepath1> <filepath2>")
+  .option("-f, --format [type]", "output format", "stylish")
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing ${filepath1} and ${filepath2} with format ${program.format}`);
+  });
+  program.parse(process.argv);
+  if (!process.argv.slice(2).length || program.help) {
+    program.outputHelp();
+  }
