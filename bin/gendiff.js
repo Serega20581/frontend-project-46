d485b232edfe4d144a00a7a219b167b58f25569a
SergeiToptunov <toptunov.2005@mail.ru>
2024-08-27 13:18:47 +0300

    step1

---
+++ bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+
+const program = new Command();
+
+program
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("1.0.0")
+  .option("-f, --format <type>", "output format", "stylish")
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
+  })
+  .parse();
---
+++ index.js
@@ -0,0 +1,6 @@
+function genDiff(filepath1, filepath2) {
+
+  return 'Difference between file';
+}
+
+export default genDiff;
