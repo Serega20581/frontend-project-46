f1b5426bc8a02bdb0786f55c77f657f39299fcab
SergeiToptunov <toptunov.2005@mail.ru>
2024-08-29 15:20:10 +0300

    refactoring

--- fileParser.js
+++ fileParser.js
@@ -8,31 +8,32 @@ const parseFile = (filepath) => {
   return JSON.parse(fileContent);
 };
 
+const generateDiffLine = (key, value, type) => `${type} ${key}: ${value}`;
+
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-
   const sortedKeys = _.sortBy(keys);
 
   const diff = sortedKeys.map((key) => {
     if (!Object.prototype.hasOwnProperty.call(data2, key)) {
-      return `- ${key}: ${data1[key]}`;
+      return generateDiffLine(key, data1[key], '-');
     }
     if (!Object.prototype.hasOwnProperty.call(data1, key)) {
-      return `+ ${key}: ${data2[key]}`;
+      return generateDiffLine(key, data2[key], '+');
     }
     if (data1[key] !== data2[key]) {
       return [
-        `- ${key}: ${data1[key]}`,
-        `+ ${key}: ${data2[key]}`,
+        generateDiffLine(key, data1[key], '-'),
+        generateDiffLine(key, data2[key], '+'),
       ].join('\n');
     }
-    return `${key}: ${data1[key]}`;
+    return   `${key}: ${data1[key]}`;
   }).filter(Boolean).join('\n');
 
-  return `{\n${diff.replace(/^/gm, ' ')}\n}`;
+  return `{\n${diff}\n}`;
 };
 
 export default genDiff;
